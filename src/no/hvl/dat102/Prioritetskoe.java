ee4a6cd8fe86fc0aaecfb8a86d261db54de4708b
Erik <eskram2001@gmail.com>
2022-03-24 10:23:26 +0100

    Initial commit

---
+++ src/no/hvl/dat102/Prioritetskoe.java
@@ -0,0 +1,39 @@
+package no.hvl.dat102;
+
+public class Prioritetskoe<T extends Comparable<? super T>> implements PrioritetskoeInterface<T> {
+	private MaksHaugInterface<T> h;
+	
+	@Override
+	public void leggTil(T nyttElement) {
+		// fyll inn
+	}
+
+	@Override
+	public T fjern() {
+		// fyll inn
+		return null;
+	}
+
+	@Override
+	public T peek() {
+		// fyll inn
+		return null;
+	}
+
+	@Override
+	public boolean erTom() {
+		// fyll inn
+		return false;
+	}
+	
+	@Override
+	public int antall() {
+		// fyll inn
+		return 0;
+	}
+
+	@Override
+	public void toem() {
+		
+	}
+}
